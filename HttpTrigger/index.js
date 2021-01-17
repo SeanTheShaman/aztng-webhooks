525ae9b7517d8812ba616bb71f98bfba5b23388e
sean <srcox86@gmail.com>
2021-01-17 10:22:59 -0600

    Updated webhook receiver to parse event info.

--- HttpTrigger/index.js
+++ HttpTrigger/index.js
@@ -1,13 +1,11 @@
-module.exports = async function (context, req) {
-    context.log('JavaScript HTTP trigger function processed a request.');
-
-    const name = (req.query.name || (req.body && req.body.name));
-    const responseMessage = name
-        ? "Hello, " + name + ". This HTTP triggered function executed successfully."
-        : "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.";
-
+if (req.body.pages[0].title){
     context.res = {
-        // status: 200, /* Defaults to 200 */
-        body: responseMessage
+        body: "Page is " + req.body.pages[0].title + ", Action is " + req.body.pages[0].action + ", Event Type is " + req.headers['x-github-event']
     };
+}
+else {
+    context.res = {
+        status: 400,
+        body: ("Invalid payload for Wiki event")
+    }
 }
