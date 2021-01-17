b3d16765ab8804e254ccc91fd070d5421fc9777d
sean <srcox86@gmail.com>
2021-01-17 10:47:29 -0600

    Added try catch block

--- HttpTrigger/index.js
+++ HttpTrigger/index.js
@@ -1,5 +1,8 @@
 module.exports = async function (context, req) {
 
+
+    try 
+    {
         if (req.body.pages[0].title){
             context.res = {
                 body: "Page is " + req.body.pages[0].title + ", Action is " + req.body.pages[0].action + ", Event Type is " + req.headers['x-github-event']
@@ -9,7 +12,13 @@ module.exports = async function (context, req) {
             context.res = {
                 status: 400,
                 body: ("Invalid payload for Wiki event")
-            }
+            };
         }
+    }
+    catch(error)
+    {
+        context.res = {status:500, body:error } 
+    }
+
 
 }
